9d0cd16c6378f5e623b4d2544de2a5e06d66d3a4
David <thefindley@gmail.com>
2013-10-08 19:41:05 -0500

    Extended JFrame and added implements for appropriate interfaces. The groundwork for this class.

--- ProjectCorki/src/info/geared/corki/ClientFrame.java
+++ ProjectCorki/src/info/geared/corki/ClientFrame.java
@@ -1,6 +1,79 @@
 package info.geared.corki;
 
-public class ClientFrame
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class ClientFrame extends JFrame implements SessionUpdate, KeyListener, ActionListener
 {
+
+	private static final long serialVersionUID = 1L;
 	
+	private ChatSession session;
+	
+	/* All JComponents fields here. */
+
+	ClientFrame(ChatSession session)
+	{
+		buildUI();
+	}
+
+	/** This method is called in the constructor and will piece together
+	 * all of the components to correctly form the layout.
+	 */
+	private void buildUI()
+	{
+		
+	}
+
+	/** This method is called by the session when a user enters or leaves
+	 * the room. It will get the updated user list from the session and
+	 * then update the UI.
+	 */
+	public void updateUserList()
+	{
+		
+	}
+
+	/** This method is called by the session when there is a new chat
+	 * message has been received. It will in turn get the new message(s)
+	 * from the session and put them into the history TextArea.
+	 */
+	public void updateChat()
+	{
+		
+	}
+
+	/** This method listens for the user to click the send button.
+	 * It will send the message in the input area (if it is not empty)
+	 * through the session, and also print it locally to the history
+	 * TextArea
+	 */
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
+
+	/** This method listens for the user to press "Enter" to send
+	 * a message. It will send the message through the session, and
+	 * also print it locally to the history TextArea.
+	 */
+	public void keyPressed(KeyEvent e)
+	{
+		
+	}
+
+	/* Do nothing. */
+	public void keyReleased(KeyEvent e)
+	{
+	}
+
+	/* Do nothing. */
+	public void keyTyped(KeyEvent e)
+	{
+	}
+
 }
