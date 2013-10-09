78f4257f44ec95d595aa19bad7565ae043dea1fb
David <thefindley@gmail.com>
2013-10-08 19:42:53 -0500

    Extended JFrame and implemented appropriate interfaces. Groundwork for this class.

--- ProjectCorki/src/info/geared/corki/LoginFrame.java
+++ ProjectCorki/src/info/geared/corki/LoginFrame.java
@@ -1,6 +1,44 @@
 package info.geared.corki;
 
-public class LoginFrame
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame implements ActionListener
 {
+	private static final long serialVersionUID = 1L;
+	
+	/* All JComponents fields here. */
+	// 4 text areas
+	// 4 labels
+	// 1 button
+	
+	LoginFrame()
+	{
+		
+		buildUI();
+	}
+	
+	private void buildUI()
+	{
+		
+	}
+
+	/** This handler will be called when the user clicks the Login
+	 * button. The method will verify the user entered valid information
+	 * and alert them if there is a problem. It will attempt to construct
+	 * a ChatSession, and if it is successful, then it will construct a
+	 * ClientFrame and dispose.
+	 */
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
  
 }
