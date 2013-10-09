e87e11a6264a8e5753f5fe7f9fe17a8e016b94a5
David <thefindley@gmail.com>
2013-10-09 02:03:21 -0500

    Wrote receiving net code in run method. Added new field "Sender send" that can be used to send messages to the server on a different thread. Added open() and close() methods to connect and disconnect from the server.

--- ProjectCorki/src/info/geared/corki/ChatSession.java
+++ ProjectCorki/src/info/geared/corki/ChatSession.java
@@ -1,6 +1,8 @@
 package info.geared.corki;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -8,25 +10,35 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
-
 public class ChatSession implements Runnable
 {
-	public enum Status {DISCONNECTED, CONNECTED, UNKNOWN_HOST, IO_EXCEPTION, NO_MD5, NO_UTF8, NOT_INI, NAME_TAKEN}
+	public enum Status
+	{
+		DISCONNECTED, CONNECTED, UNKNOWN_HOST, IO_EXCEPTION, NO_MD5, NO_UTF8, NOT_INI, NAME_TAKEN
+	}
+
 	protected static final int DEFAULT_PORT = 73195;
-	
+	protected static final int TIMEOUT = 3000;
+
 	protected String hostname;
 	protected String username;
 	protected int port;
 	protected byte[] hashedPassword;
 
 	protected ArrayList<ChatSessionListener> listeners;
+	protected Sender sender;
 	protected Socket socket;
+	protected Thread thread;
 
 	protected Status status;
+	protected boolean isClosed;
 
 	ChatSession(String host, String username, String password)
 	{
 		status = Status.NOT_INI;
+		isClosed = true;
+		thread = new Thread(this);
+		
 		if (host.contains(":"))
 		{
 			hostname = host.split(":")[0];
@@ -61,6 +73,8 @@ public class ChatSession implements Runnable
 		try
 		{
 			socket = new Socket(hostname, port);
+			socket.setSoTimeout(TIMEOUT);
+			sender = new Sender(socket.getOutputStream());
 		}
 		catch (UnknownHostException e)
 		{
@@ -74,23 +88,103 @@ public class ChatSession implements Runnable
 			status = Status.IO_EXCEPTION;
 			return;
 		}
-		
+
 		if (isNameTaken())
 		{
-			status = Status.NAME_TAKEN; 
+			status = Status.NAME_TAKEN;
 			return;
 		}
-		
+
 		status = Status.DISCONNECTED;
 	}
 	
-	protected boolean isNameTaken()
+	public boolean send(String message)
 	{
-		return false;
+		return sender.send(message);
+	}
+	
+	public boolean open()
+	{
+		/* Check if the session is already open. */
+		if (isClosed == false)
+			return false;
+		
+		/* If the session status is not DISCONNECTED, then the
+		 * session is not ready to be opened. */
+		if (status != Status.DISCONNECTED)
+			return false;
+		
+		/* Check if the main receiving thread is already running. */
+		if (thread.isAlive())
+			return false;
+		else
+			thread.start();
+		
+		/* Try to send connect command to server. */
+		if (!sender.send("CONNECT:"+username))
+			return false;
+		
+		isClosed = false;
+		status = Status.CONNECTED;
+		return true;
+	}
+	
+	public void close()
+	{
+		sender.send("DISCONNECT");
+		isClosed = true;
+		status = Status.DISCONNECTED;
+	}
+	
+	public void addChatSessionListener(ChatSessionListener csl)
+	{
+		listeners.add(csl);
+	}
+	
+	public Status getStatus()
+	{
+		return status;
 	}
 
 	public void run()
 	{
-		
+		BufferedReader in;
+		try
+		{
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			while (isClosed == false)
+			{
+				/* Try to read a line from the socket. */
+				String line = in.readLine();
+				
+				/* If the read timedout then line will be empty or null. */
+				if (line == null || line.isEmpty())
+					continue;
+				
+				/* When a line is read update the listeners. */
+				for(ChatSessionListener listener : listeners)
+				{
+					listener.update(line);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	protected synchronized boolean isNameTaken()
+	{
+		return false;
+	}
+
+	protected synchronized boolean connect()
+	{
+		return false;
+	}
+
+	protected synchronized void disconnect()
+	{
 	}
 }
