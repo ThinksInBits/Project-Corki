822cbd44496370bd211413f8dba8be5ebf0f6693
David <thefindley@gmail.com>
2013-10-10 17:25:08 -0500

    Made ClientWorker a subclass of Client.

--- src/info/geared/corki/server/Client.java
+++ src/info/geared/corki/server/Client.java
@@ -1,6 +1,33 @@
 package info.geared.corki.server;
 
+import java.net.Socket;
+
 public class Client
 {
+	public class ClientWorker implements Runnable
+	{
+			
+		public void run()
+		{
+			
+		}
 
+	}
+
+	
+	protected Socket socket;
+	protected String name;
+	protected ClientWorker worker;
+	protected boolean running;
+	
+	public Client(Socket socket)
+	{
+		running = false;
+		this.socket = socket;
+	}
+	
+	public void disconnect()
+	{
+		running = false;
+	}
 }
